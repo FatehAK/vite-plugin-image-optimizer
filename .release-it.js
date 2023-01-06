f1c7c7607d0690b13ee2b8c662ce3726702f6991
fa7ehak <fa7ehak@gmail.com>
2023-01-07 00:16:25 +0530

    chore: Update release-it config

--- .release-it.js
+++ .release-it.js
@@ -1,15 +1,17 @@
+// refer: https://github.com/release-it/release-it/tree/master/docs
 module.exports = {
   git: {
     requireBranch: 'main',
     commitMessage: 'chore: Release v${version}',
     tagName: 'v${version}',
-    requireCleanWorkingDir: true,
+    requireCommits: true, // require commits since last tag
+    requireCleanWorkingDir: true, // exits if local not upto date with remote or if workdir is unclean
   },
   github: {
-    release: true,
-    draft: true,
-    commitArgs: ['-S'],
-    tagArgs: ['-s'],
+    release: true, // creates a github release
+    draft: true, // github releases are only drafted, confirm the draft in github releases page to publish it
+    commitArgs: ['-S'], // creates gpg signed commits
+    tagArgs: ['-s'], // creates gpg signed tags
     releaseName: '✨ v${version}',
     assets: ['tar/*.tgz'],
   },
@@ -17,9 +19,10 @@ module.exports = {
     publish: true,
   },
   hooks: {
-    'before:init': ['if [ "$(git log $(git describe --tags --abbrev=0)..HEAD)" = "" ]; then exit 1; fi;', 'pnpm lint'],
+    // runs lint before releasing
+    'before:init': ['pnpm lint'],
+    // build the package and generate a tarball for use in github releases
     'after:bump': 'pnpm build && pnpm tarball',
-    'after:release':
-      'echo Successfully created a release v${version} for ${repo.repository}. Please add release notes when necessary and publish it!',
+    'after:release': 'echo Successfully created a release v${version} for ${repo.repository}. Please add release notes and publish it!',
   },
 };
