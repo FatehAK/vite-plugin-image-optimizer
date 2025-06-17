06e154f01c329474a6bfa0b6e3b95a94d4ebbd9f
Fateh <fa7ehak@gmail.com>
2025-06-17 16:23:19 +0530

    fix(minor): Fix default svgo config

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,35 @@
+import type { Config as SVGOConfig } from 'svgo';
+import type { PngOptions, JpegOptions, TiffOptions, GifOptions, WebpOptions, AvifOptions } from 'sharp';
+
 export const VITE_PLUGIN_NAME = 'vite-plugin-image-optimizer';
+
+const SVGO_CONFIG: SVGOConfig = {
+  multipass: true,
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          cleanupNumericValues: false,
+          removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+          cleanupIds: {
+            minify: false,
+            remove: false,
+          },
+          convertPathData: false,
+        },
+      },
+    },
+    'sortAttrs',
+    {
+      name: 'addAttributesToSVGElement',
+      params: {
+        attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+      },
+    },
+  ],
+};
+
 export const DEFAULT_OPTIONS = {
   logStats: true,
   ansiColors: true,
@@ -6,59 +37,34 @@ export const DEFAULT_OPTIONS = {
   exclude: undefined,
   include: undefined,
   test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
-  svg: {
-    multipass: true,
-    plugins: [
-      {
-        name: 'preset-default',
-        params: {
-          overrides: {
-            cleanupNumericValues: false,
-            removeViewBox: false, // https://github.com/svg/svgo/issues/1128
-          },
-          cleanupIDs: {
-            minify: false,
-            remove: false,
-          },
-          convertPathData: false,
-        },
-      },
-      'sortAttrs',
-      {
-        name: 'addAttributesToSVGElement',
-        params: {
-          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
-        },
-      },
-    ],
-  },
+  svg: SVGO_CONFIG,
   png: {
     // https://sharp.pixelplumbing.com/api-output#png
     quality: 100,
-  },
+  } as PngOptions,
   jpeg: {
     // https://sharp.pixelplumbing.com/api-output#jpeg
     quality: 100,
-  },
+  } as JpegOptions,
   jpg: {
     // https://sharp.pixelplumbing.com/api-output#jpeg
     quality: 100,
-  },
+  } as JpegOptions,
   tiff: {
     // https://sharp.pixelplumbing.com/api-output#tiff
     quality: 100,
-  },
+  } as TiffOptions,
   // gif does not support lossless compression
   // https://sharp.pixelplumbing.com/api-output#gif
-  gif: {},
+  gif: {} as GifOptions,
   webp: {
     // https://sharp.pixelplumbing.com/api-output#webp
     lossless: true,
-  },
+  } as WebpOptions,
   avif: {
     // https://sharp.pixelplumbing.com/api-output#avif
     lossless: true,
-  },
+  } as AvifOptions,
   cache: false,
   cacheLocation: undefined,
 };
